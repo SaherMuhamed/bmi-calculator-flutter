030f297d989ad9d1304bbcfbab37f982e58d8cd5
Saher Mohamed <sahermuhamed21@gmail.com>
2023-07-03 03:32:49 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main(){
-
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
+
   runApp(const BMICalculator());
 }
 
